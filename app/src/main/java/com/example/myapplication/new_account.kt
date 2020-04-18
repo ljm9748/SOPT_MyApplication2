2117798cb6d6ab895c256c786a6d2ee84ffdb1a5
Jungmin <ljm9748@naver.com>
2020-04-18 13:39:15 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/new_account.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class new_account : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_account)
+    }
+}
