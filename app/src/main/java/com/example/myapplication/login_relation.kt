49143ad22fb3b5ff1a0c3710385269ba0b370931
Jungmin <ljm9748@naver.com>
2020-04-18 14:38:37 +0900

    2nd

---
+++ app/src/main/java/com/example/myapplication/login_relation.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class login_relation : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_relation)
+
+
+    }
+
+
+}
